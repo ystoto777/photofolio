1206b7680be9dabca1e609d4970194b47fc386bc
ystoto777 <77279899+ystoto777@users.noreply.github.com>
2021-01-11 23:16:34 +0900

    Delete mocloc.js

--- mocloc.js
+++
@@ -1,9 +0,0 @@
-var $clickme = $('.clickme'),
-    $box = $('.box');
-
-$box.hide();
-
-$clickme.click( function(e) {
-    $(this).text(($(this).text() === '' ? '<1>' : 'Hide')).next('.box').slideToggle();;
-    e.preventDefault();
-});
